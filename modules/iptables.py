f6ac84e3c82915e5c72fb000d75107bd1354c5af
osielczakd <osielczakd@gmail.com>
2015-07-02 17:36:29 +0200

    work in progress (STILL UNUSABLE)

--- modules/iptables.py
+++ modules/iptables.py
@@ -3,40 +3,50 @@
 
 import re
 import os
-import pipes
 import tempfile
 
 DOCUMENTATION="""
 ---
 module: iptables
 author:
-	- Dabiel Osielczak (Daniel.Osielczak@opitz-consulting.com)
+	- Daniel Osielczak (Daniel.Osielczak@opitz-consulting.com)
 	
 short_description: Manipulates iptables entries by rewriting /etc/sysconfig/iptables
 """
 
-def write_header(module):
-	HEADER = 'This file is maintained by Ansible\nPlease refrain from making any changes manually as those WILL BE OVERWRITTEN WITH NEXT UPDATE!\nInsted, plaese make your changes in Ansible playbook/host_vars file'
-	f = os.fopen('/etc/sysconfig/iptables.test', 'wb')
+def write_header(module, conffile):
+	HEADER = '##############################################################\n#\n#This file is maintained by Ansible\n#Please refrain from making any changes manually as those WILL BE OVERWRITTEN WITH NEXT UPDATE!\n#Instead, plaese make your changes in Ansible playbook/host_vars file\n##############################################################\n'
+	tmpfd, tmpfile = tempfile.mkstemp()
+	f = os.fdopen(tmpfd,'wb')
 	f.write(HEADER)
+	f.close()
 	
+	if validate:
+		module.atomic_move(tmpfile, conffile)
+	else:
+		module.atomic_move(tmpfile, conffile)
 
 def write_entry(module, chain, state, prot, port, action):
-
+	pass
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-                enabled=dict(default=True, choices=['True', 'False']),
-                chain=dict(default=None, type='str'),
-                state=dict(default='NEW', type='str'),
-                prot=dict(default='tcp', type='str'),
-                port=dict(default=None, type='str'),
-                action=dict(default='ACCEPT', type='str'),
-                ),
-        supports_check_mode=True
-    )
-
-    params = module.params
+	module = AnsibleModule(
+		argument_spec=dict(
+			enabled=dict(required=True, default=True, choices=['True', 'False']),
+			file=dict(default='/etc/sysconfig/iptables.test', type='str'),
+			chain=dict(default=None, type='str'),
+			state=dict(default='NEW', type='str'),
+			prot=dict(default='tcp', type='str'),
+			port=dict(default=None, type='str'),
+			action=dict(default='ACCEPT', type='str')
+			),
+		add_file_common_args=True,
+		supports_check_mode=True
+	)
+	
+	params = module.params
+	file = module.params.get('file')
+	write_header(module, file)
+    
 
 # import module snippets
 from ansible.module_utils.basic import *
